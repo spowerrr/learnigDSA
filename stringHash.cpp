270d0fecf235d93205ecd98b2a445248ff6b4f38
spowerrr <puspopuspo520@gmail.com>
2024-04-20 19:17:53 +0600

    hash

---
+++ stringHash.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    //compute
+    int hash[256]={0};
+    for (int i = 0; i < s.size(); i++)
+    {
+        hash[s[i]]+=1;
+    }
+    
+    int q;
+    cin>>q;
+    while(q--){
+        char c;
+        cin>>c;
+        //fetch
+        cout<<hash[c]<<" times"<<endl;
+    }
+    return 0;
+}
